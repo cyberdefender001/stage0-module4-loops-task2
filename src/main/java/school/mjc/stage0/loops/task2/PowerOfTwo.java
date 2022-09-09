e88cf476a88532568de30c37b5ba6c5f08ee53f4
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-09 16:27:09 +0500

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int temp = 0;
+        int pow = 0;
+        while (temp <= power) {
+            pow = 2 * temp;
+            System.out.println(pow);
+            temp++;
+        }
     }
 }
