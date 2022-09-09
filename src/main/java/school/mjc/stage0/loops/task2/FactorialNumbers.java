721db36f1f619ec337321eea8aed901894e09b70
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-09 15:58:32 +0500

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int temp = 1;
+        int count = 1;
+        int mult = 1;
+        while (temp <= printToInclusive) {
+            while (count <= temp) {
+                mult *= count;
+                count++;
+            }
+            System.out.println(mult);
+            temp++;
+        }
     }
 }
