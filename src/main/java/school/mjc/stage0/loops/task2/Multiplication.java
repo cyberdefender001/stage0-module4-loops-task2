f90113a7c467a4aef416d159517052778d0a4d1d
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-09 16:07:44 +0500

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int temp = 1;
+        int mult = 1;
+        while (temp < multiplyByAndToInclusive) {
+            mult *= temp;
+            System.out.println(mult);
+            temp ++;
+        }
     }
 }
