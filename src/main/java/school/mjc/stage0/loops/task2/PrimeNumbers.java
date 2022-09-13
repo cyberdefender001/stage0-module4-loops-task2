4bf895165c576e324e53c233b83204ff7e147d52
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-13 15:50:56 +0500

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+            int i, number = 1, count;
+
+            System.out.println(" From 1 to 100 are : ");
+            while(number <= 100)
+            {
+                count = 0;
+                i = 2;
+                while(i <= number/2 )
+                {
+                    if(number % i == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                    i++;
+                }
+                if(count == 0 && number != 1 )
+                {
+                    System.out.println(number + " ");
+                }
+                number++;
+            }
+        }
     }
 }
